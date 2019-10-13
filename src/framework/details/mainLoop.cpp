e0b128fde5cd9ca17e5a4e413d2749d10f707eab
Przemek <przempore@gmail.com>
2019-10-13 15:24:31 +0200

    forgot to add add_subdirectory(third_party)

--- src/framework/details/mainLoop.cpp
+++ src/framework/details/mainLoop.cpp
@@ -1,7 +1,5 @@
 #include "mainLoop.hpp"
 
-#include <memory>
-
 #include <SFML/Graphics.hpp>
 #include "graphics.hpp"
 #include "i_game_application.hpp"
@@ -19,21 +17,15 @@ bool mainLoop(const GraphicsWindowConfig& config) {
   while (window.isOpen()) {
     sf::Event event{};
     while (window.pollEvent(event)) {
-      // check the type of the event...
       switch (event.type) {
-        // window closed
         case sf::Event::Closed:window.close();
           return true;
-
-          // key pressed
         case sf::Event::KeyPressed:
           game_application->onInput({Game::Input::KeyState::Down, Game::Input::Key(event.key.code)});
           break;
-
         case sf::Event::KeyReleased:
           game_application->onInput({Game::Input::KeyState::Up, Game::Input::Key(event.key.code)});
           break;
-
         default:break;
       }
     }
