9e388874580154a27632de5f9b7c93b9304ae30d
Przemek <przempore@gmail.com>
2020-01-04 15:31:26 +0100

    override interface method server.go

--- src/framework/network/golang_server/server_main/server.go
+++ src/framework/network/golang_server/server_main/server.go
@@ -8,7 +8,8 @@ import (
 	"fmt"
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
-	//"google.golang.org/grpc/codes"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 	"log"
 	"net"
 
@@ -19,26 +20,26 @@ const (
 	port = ":50051"
 )
 
-//const (
-//Black       = 0
-//White       = 1
-//Red         = 2
-//Green       = 3
-//Blue        = 4
-//Yellow      = 5
-//Magenta     = 6
-//Cyan        = 7
-//Transparent = 8
-//)
+const (
+	Black       = 0
+	White       = 1
+	Red         = 2
+	Green       = 3
+	Blue        = 4
+	Yellow      = 5
+	Magenta     = 6
+	Cyan        = 7
+	Transparent = 8
+)
 
-//type Point struct {
-//x float32
-//y float32
-//}
+type Point struct {
+	x float32
+	y float32
+}
 
-//type Rectangles struct {
-//id uint32
-//}
+type Rectangles struct {
+	id uint32
+}
 
 type Vehicles struct {
 	vehicles []uint32
@@ -52,8 +53,34 @@ type server struct {
 func (s *server) GetNewId(ctx context.Context, req *empty.Empty) (*pb.Id, error) {
 	fmt.Printf("\n\tGetNewId\n")
 	return &pb.Id{Id: uint32(len(s.vehicles))}, nil
-	//return &pb.Id{Id: 32}, nil
+	//return nil, status.Errorf(codes.Unimplemented, "method GetNewId not implemented")
 }
+
+func (s *server) GetVehicles(ctx context.Context, req *empty.Empty) (*pb.Vehicles, error) {
+	fmt.Printf("\n\tGetVehicles\n")
+	return nil, status.Errorf(codes.Unimplemented, "method GetVehicles not implemented")
+}
+
+func (s *server) UpdateVehicle(ctx context.Context, req *pb.VehicleWithId) (*empty.Empty, error) {
+	fmt.Printf("\n\tUpdateVehicle\n")
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateVehicle not implemented")
+}
+
+func (s *server) RegisterVehicle(ctx context.Context, req *pb.VehicleWithId) (*pb.Id, error) {
+	fmt.Printf("\n\tRegisterVehicle\n")
+	return nil, status.Errorf(codes.Unimplemented, "method RegisterVehicle not implemented")
+}
+
+func (s *server) UnregisterVehicle(ctx context.Context, req *pb.Id) (*pb.Id, error) {
+	fmt.Printf("\n\tUnregisterVehicle\n")
+	return nil, status.Errorf(codes.Unimplemented, "method UnregisterVehicle not implemented")
+}
+
+//func (s *server) GetNewId(ctx context.Context, req *empty.Empty) (*pb.Id, error) {
+//fmt.Printf("\n\tGetNewId\n")
+//return &pb.Id{Id: uint32(len(s.vehicles))}, nil
+//return &pb.Id{Id: 32}, nil
+//}
 
 //func (s *server) GetVehicles(ctx context.Context, req *empty.Empty) (*Vehicles, error) {
 //fmt.Printf("GetVehicles\n")
