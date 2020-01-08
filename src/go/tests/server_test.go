63917070021f8330d058aa7ffd14be5f720e9d74
Przemek <przempore@gmail.com>
2020-01-08 22:16:10 +0100

    add go unit tests

--- src/go/tests/server_test.go
+++ src/go/tests/server_test.go
@@ -4,9 +4,9 @@ import (
 	pb "../golang_server/car_crash"
 	utils "../golang_server/utilities"
 	"fmt"
-	"google.golang.org/grpc"
-	"log"
-	"net"
+	//"google.golang.org/grpc"
+	//"log"
+	//"net"
 	//"os"
 	"testing"
 )
@@ -21,25 +21,111 @@ func TestTest(t *testing.T) {
 	}
 }
 
-func TestServer(t *testing.T) {
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+func TestCopyToGrpc(t *testing.T) {
+	rectangeToTranslate := utils.Rectangle{
+		Id: 42,
+		Position: utils.Point{
+			X: 123,
+			Y: 321,
+		},
+		Angle: 42.1,
+		Dimension: utils.Point{
+			X: 432,
+			Y: 987,
+		},
+		Color: utils.RectangleColor_Red,
+		Origin: utils.Point{
+			X: 765,
+			Y: 567,
+		},
 	}
-	s := grpc.NewServer()
-	carCrashServer := new(utils.Server)
-	carCrashServer.Vehicles = make(map[uint32]utils.Rectangle)
-	pb.RegisterCarCrashServer(s, carCrashServer)
-	fmt.Printf("Serving at tcp%s", port)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	got := utils.CopyToGrpc(&rectangeToTranslate)
+
+	expected := &pb.Rectangle{
+		Id: 42,
+		Position: &pb.Point{
+			X: 123,
+			Y: 321,
+		},
+		Angle: 42.1,
+		Dimension: &pb.Point{
+			X: 432,
+			Y: 987,
+		}, Color: pb.Rectangle_Red,
+		Origin: &pb.Point{
+			X: 765,
+			Y: 567,
+		},
+	}
+	fmt.Printf("got: %+v\n", got)
+	fmt.Printf("expected: %+v\n", expected)
+	if got.Id != expected.Id ||
+		got.Position.X != expected.Position.X ||
+		got.Position.Y != expected.Position.Y ||
+		got.Angle != expected.Angle ||
+		got.Dimension.X != expected.Dimension.X ||
+		got.Dimension.Y != expected.Dimension.Y ||
+		got.Color != expected.Color ||
+		got.Origin.X != expected.Origin.X ||
+		got.Origin.Y != expected.Origin.Y {
+		t.Errorf("got '%+v' want '%+v'", got, expected)
 	}
 }
 
-//func testMain(m *testing.M) {
-//log.Println("Do stuff BEFORE the tests!")
-//exitVal := m.Run()
-//log.Println("Do stuff AFTER the tests!")
+func TestCopyFromGrpc(t *testing.T) {
+	rectangeToTranslate := pb.Rectangle{
+		Id: 42,
+		Position: &pb.Point{
+			X: 123,
+			Y: 321,
+		},
+		Angle: 42.1,
+		Dimension: &pb.Point{
+			X: 432,
+			Y: 987,
+		},
+		Color: pb.Rectangle_Yellow,
+		Origin: &pb.Point{
+			X: 765,
+			Y: 567,
+		},
+	}
+	got := utils.CopyFromGrpc(&rectangeToTranslate)
 
-//os.Exit(exitVal)
+	expected := utils.Rectangle{
+		Id: 42,
+		Position: utils.Point{
+			X: 123,
+			Y: 321,
+		},
+		Angle: 42.1,
+		Dimension: utils.Point{
+			X: 432,
+			Y: 987,
+		}, Color: utils.RectangleColor_Yellow,
+		Origin: utils.Point{
+			X: 765,
+			Y: 567,
+		},
+	}
+	fmt.Printf("got: %+v\n", got)
+	fmt.Printf("expected: %+v\n", expected)
+	if got != expected {
+		t.Errorf("got '%+v' want '%+v'", got, expected)
+	}
+}
+
+//func TestServer(t *testing.T) {
+//lis, err := net.Listen("tcp", port)
+//if err != nil {
+//log.Fatalf("failed to listen: %v", err)
+//}
+//s := grpc.NewServer()
+//carCrashServer := new(utils.Server)
+//carCrashServer.Vehicles = make(map[uint32]utils.Rectangle)
+//pb.RegisterCarCrashServer(s, carCrashServer)
+//fmt.Printf("Serving at tcp%s", port)
+//if err := s.Serve(lis); err != nil {
+//log.Fatalf("failed to serve: %v", err)
+//}
 //}
